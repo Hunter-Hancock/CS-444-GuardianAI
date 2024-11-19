e5fe17138363230e17516c65a4fbfe130ff1b8d1
Hunter Hancock <hunterhancock1141@gmail.com>
2024-11-19 13:17:35 -0600

    fix: corrected the api url in service-worker script

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.contentScriptQuery == "test") {
 
-    const url = "cs-444-guardianai-production.up.railway.app"
+    const url = "https://cs-444-guardianai-production.up.railway.app"
     fetch(url).then(response => response.json())
       .then(json => sendResponse(json))
 
